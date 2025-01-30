2332294808cf20377b07644e849e5cc56fdf8c31
Metin Yalcinkaya <metinyalcinkaya@hotmail.com.au>
2025-01-30 21:25:17 +1100

    Added codecompanion.nvim and vgit to test

---
+++ lua/plugins/codecompanion.lua
@@ -0,0 +1,28 @@
+return {
+  {
+    "olimorris/codecompanion.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-treesitter/nvim-treesitter",
+    },
+    event = "VeryLazy",
+    config = function()
+      require("codecompanion").setup({
+        strategies = {
+          chat = {
+            adapter = "anthropic",
+          },
+        },
+        adapters = {
+          anthropic = function()
+            return require("codecompanion.adapters").extend("anthropic", {
+              env = {
+                api_key = "cmd:op read op://API/anthropic/credential --no-newline",
+              },
+            })
+          end,
+        },
+      })
+    end,
+  },
+}
