37e45bb6140d24b6a3ff34a3075fdb608bb64b61
Metin Yalcinkaya <metinyalcinkaya@hotmail.com.au>
2025-12-01 15:33:13 +1100

    rid config of java

--- lua/config/lazy-plugins.lua
+++ lua/config/lazy-plugins.lua
@@ -1,9 +1,8 @@
 require("lazy").setup({
     -- This auto imports all plugins from the given folders, so you don't have to call each plugin individually
-    { import = "plugins.LSP" },
     { import = "plugins" },
     { import = "plugins.Notes" },
-    { import = "plugins.Java" },
+    { import = "plugins.LSP" },
 }, {
     performance = {
         rtp = {
